7dd37a0f9ae6001cbcbf100afafef922ebb94101
Henry Denny <henry.denny12@gmail.com>
2025-10-03 21:43:24 +0100

    Add plotters crate boilerplate for graphing.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use plotters::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let drawing_area = BitMapBackend::new("random-walk.png", (900, 900))
+        .into_drawing_area()
+        .margin(30, 30, 30, 30);
+
+    drawing_area.fill(&WHITE).unwrap();
+
+    let mut chart = ChartBuilder::on(&drawing_area)
+        .caption("Random Walk", ("sans-serif", 40).into_font())
+        // Set the size of the label region
+        .x_label_area_size(20)
+        .y_label_area_size(20)
+        .build_cartesian_2d(-10..10, -10..10)
+        .unwrap();
+
+    chart.configure_mesh().draw().unwrap();
+
+    chart
+        .draw_series(LineSeries::new(
+            vec![(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (3, 2)],
+            &RED,
+        ))
+        .unwrap();
 }
