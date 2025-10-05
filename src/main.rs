06797fb4d6eb26bb1c79d095fc98621f962742e3
Henry Denny <henry.denny12@gmail.com>
2025-10-05 19:03:57 +0100

    Implement drawing of random walk onto bitmap gif context.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use plotters::prelude::*;
 
+use random_walk::{Point, RandomWalk};
+
 pub mod random_walk;
 
 fn main() {
-    let drawing_area = BitMapBackend::new("random-walk.png", (900, 900))
+    let drawing_area = BitMapBackend::gif("random-walk.gif", (900, 900), 500)
+        .expect("Should be able to create new gif")
         .into_drawing_area()
         .margin(30, 30, 30, 30);
 
@@ -12,17 +15,21 @@ fn main() {
     let mut chart = ChartBuilder::on(&drawing_area)
         .caption("Random Walk", ("sans-serif", 40).into_font())
         // Set the size of the label region
-        .x_label_area_size(20)
-        .y_label_area_size(20)
         .build_cartesian_2d(-10..10, -10..10)
         .unwrap();
 
     chart.configure_mesh().draw().unwrap();
 
-    chart
-        .draw_series(LineSeries::new(
-            vec![(0, 0), (1, 0), (2, 0), (2, 1), (2, 2), (3, 2)],
-            &RED,
-        ))
-        .unwrap();
+    let rw: Vec<Point> = RandomWalk::new(25).collect();
+    rw.windows(2).for_each(|line| {
+        chart
+            .draw_series(LineSeries::new(
+                line.iter().copied(),
+                Into::<ShapeStyle>::into(RED).stroke_width(2).filled(),
+            ))
+            .expect("Should be able to plot random walk");
+        drawing_area
+            .present()
+            .expect("Should be able to render frame");
+    });
 }
